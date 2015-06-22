d9057ef2836432594e02321c51248b88956e53a7
Micky Abir <micky.abir@gmail.com>
2015-06-22 04:29:12 -0500

    initial commit

--- app.js
+++ app.js
@@ -11,5 +11,5 @@ var server = app.listen(3000, '0.0.0.0', function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('MTD Departures listening at http://%s:%s', host, port);
 });
