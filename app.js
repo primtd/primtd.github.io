fe0b49caa0cc4c8d1e2c7040a7330a39792c6144
Micky Abir <micky.abir@gmail.com>
2015-06-22 02:29:01 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.get('/config', function (req, res) {
+  res.sendFile(path.join(__dirname+'/view/config.html'));
+});
+
+var server = app.listen(3000, '0.0.0.0', function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
