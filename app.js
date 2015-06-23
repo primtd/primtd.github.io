41d18a26a060ecf35a499b220d8aaa6858a20315
Micky Abir <abir2@illinois.edu>
2015-06-23 02:51:57 +0000

    added stops scripts and handling

--- app.js
+++ app.js
@@ -2,8 +2,32 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+var fs = require('fs');
+var stops = JSON.parse(fs.readFileSync('./stops/stops.txt', 'utf8'));
+
+
+
 app.get('/config', function (req, res) {
   res.sendFile(path.join(__dirname+'/view/config.html'));
+});
+
+app.get('/stops', function(req, res) {
+	var found = false;
+	for(var i = 0; i < stops.stops.length; i++)
+	{
+		var id = "MTD" + req.query.id;
+		if(stops.stops[i].code === id)
+		{
+			res.send(stops.stops[i].stop_id);
+			found = true;
+			break;
+		}
+	}
+
+	if(found === false)
+	{
+		res.send("undefined");
+	}
 });
 
 var server = app.listen(3000, '0.0.0.0', function () {
---
+++ scripts/getStops.sh
@@ -0,0 +1,1 @@
+wget -O ~/primtd/stops/stops.txt https://developer.cumtd.com/api/v2.2/json/GetStops?key=d03b6f55fb65438588d2d989778341bd ~/primtd/stops/stops.txt
